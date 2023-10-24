97eda76174d2991e2f063e1de82d5f936e00c2e6
AjayRanaTS <design@techstriker.com>
2023-10-24 12:18:06 +0530

    About section Added!!

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero";
+import HomeAbout from "../components/HomeAbout";
 
 const Home = () => {
     return (
         <>
             <Hero />
+            <HomeAbout />
         </>
     );
   };
